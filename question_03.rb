dc228579c7daa4a2a5f62a42c5f792fac9f6c4d3
Sarah McAlear <sarah.mcalear@gmail.com>
2015-07-06 18:11:04 -0700

    answers to question 3

--- question_03.rb
+++ question_03.rb
@@ -1,27 +1,13 @@
 require 'pry'
+require 'prime'
 number = 600851475143
 
 
 def greatest_prime(num)
-  index = num
-  all_numbers = []
 
-  loop do
-    all_numbers << num - index
-    index -= 1
-    break if index == 1
-  end
+  all_primes = Prime.prime_division num
 
-  number_array = all_numbers.select do |x|
-    num % x == 0
-  end
-binding.pry
-  array = number_array.select do |y|
-   y.prime?
-  end
-
-
-  puts array.max
+  puts all_primes.max[0]
 
 end
 
